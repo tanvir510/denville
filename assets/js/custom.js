b29754eb944ee0c3bebe2caec4fb5c135b1710e7
tanvir510 <tanvirchy.dev@gmail.com>
2022-01-23 01:49:57 +0600

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,13 @@
+(function ($) {
+  "use strict";
+
+  /*****************************
+   * Banner Swiper
+   *****************************/
+  const swiper = new Swiper(".banner-container", {
+    // If we need pagination
+    pagination: {
+      el: ".banner-pagination",
+    },
+  });
+})(jQuery);
