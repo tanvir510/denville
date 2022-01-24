bb01fda5b5d47eabcf1234a2d4239925cd225938
tanvir510 <tanvirchy.dev@gmail.com>
2022-01-25 01:05:21 +0600

    New feature has been added, header PC and SP layout updated

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -28,11 +28,29 @@
   /*****************************
    * Navbar outside click
    *****************************/
-  // $(function () {
-  //   $(document).click(function (event) {
-  //     $(".navbar-collapse").collapse("hide");
-  //   });
-  // });
+  $(document).click(function (event) {
+    /// If *navbar-collapse* is not among targets of event
+    if (!$(event.target).is(".navbar-collapse *")) {
+      $(".navbar-collapse").collapse("hide");
+    }
+  });
+
+  $(function () {
+    var $window = $(window);
+    var windowsize = $window.width();
+    if (windowsize > 991) {
+      $(".dropdown").hover(
+        function () {
+          $(".dropdown").addClass("open");
+          $(".dropdown-menu").addClass("show");
+        },
+        function () {
+          $(".dropdown").removeClass("open");
+          $(".dropdown-menu").removeClass("show");
+        }
+      );
+    }
+  });
 
   /*****************************
    * Scroll Bottom to Top
