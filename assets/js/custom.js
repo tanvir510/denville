f0df481612be4a5e306d53f412bb8f5c445be902
tanvir510 <tanvirchy.dev@gmail.com>
2024-09-03 07:05:05 +0600

    FEAT: Local magic page design with responsive

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,49 +2,28 @@
   "use strict";
 
   /*****************************
-   * Menu Scroll Top
-   *****************************/
-  var fixed_top = $("header");
-  $(window).on("scroll", function () {
-    if ($(this).scrollTop() > 80) {
-      fixed_top.addClass("menu-fixed animated fadeInDown");
-    } else {
-      fixed_top.removeClass("menu-fixed animated fadeInDown");
-    }
-  });
-
-  /*****************************
    * Banner Swiper
    *****************************/
-  const swiper = new Swiper(".banner-container", {
+  const swiper = new Swiper(".testimonial-container", {
+    // Optional parameters
     loop: true,
     speed: 5000,
+    slidesPerView: 1,
+    spaceBetween: 50,
     autoplay: {
       delay: 5,
       disableOnInteraction: false,
     },
-  });
 
-  /*****************************
-   * Navbar outside click
-   *****************************/
-  $(document).click(function (event) {
-    if (!$(event.target).is(".navbar-collapse *")) {
-      $(".navbar-collapse").collapse("hide");
-    }
-  });
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+    },
 
-  $(function () {
-    $(".desktop-nav").hover(
-      function () {
-        $(".desktop-nav").addClass("open");
-        $(".desktop-nav-menu").addClass("show");
-      },
-      function () {
-        $(".desktop-nav").removeClass("open");
-        $(".desktop-nav-menu").removeClass("show");
-      }
-    );
+    // Navigation arrows
+    navigation: {
+      enabled: false,
+    },
   });
 
   /*****************************
