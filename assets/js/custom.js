6e4884f7e6bf2a07ef3595ecc97908de384a6809
tanvir510 <tanvirchy.dev@gmail.com>
2024-09-07 20:07:07 +0600

    FEAT: JS and CSS files are minified

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,31 +2,6 @@
   "use strict";
 
   /*****************************
-   * Banner Swiper
-   *****************************/
-  const swiper = new Swiper(".testimonial-container", {
-    // Optional parameters
-    loop: true,
-    speed: 5000,
-    slidesPerView: 1,
-    spaceBetween: 50,
-    autoplay: {
-      delay: 5,
-      disableOnInteraction: false,
-    },
-
-    // If we need pagination
-    pagination: {
-      el: ".swiper-pagination",
-    },
-
-    // Navigation arrows
-    navigation: {
-      enabled: false,
-    },
-  });
-
-  /*****************************
    * Scroll Bottom to Top
    *****************************/
   $(".scrollToTop").click(function () {
